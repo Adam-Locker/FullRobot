c6ce35e4fb732600440f0006e66e0778ec501dc4
Adam-Locker <adam.locker@ht-la.org>
2015-12-17 12:29:12 -0800

    Pneumatics

--- src/org/usfirst/frc/team4/robot/subsystems/Pneumatics.java
+++ src/org/usfirst/frc/team4/robot/subsystems/Pneumatics.java
@@ -1,5 +1,20 @@
 package org.usfirst.frc.team4.robot.subsystems;
 
-public class Pneumatics {
+import org.usfirst.frc.team4.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import edu.wpi.first.wpilibj.Compressor;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+public class Pneumatics extends Subsystem{
+
+	protected void initDefaultCommand() {
+	}
+	
+	 
+	
+	
 
 }
